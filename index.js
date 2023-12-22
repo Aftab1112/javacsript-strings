ad1aa2654a632fbd1156e32270edf82f4e14787d
Aftab1112 <you@example.com>
2023-12-22 22:17:37 +0530

    strings added

---
+++ index.js
@@ -0,0 +1,34 @@
+// let str = "aftab"
+// let str2 = 'Aftab'
+
+//Template Literals
+// let specialString = `This is a template literal`
+// console.log(specialString)
+
+
+// let obj ={
+//     item : "pen",
+//     price : 40,
+// }
+// console.log("the cost of ",obj.item , "is",obj.price,"rupees")
+
+// let output = `the cost of ${obj.item} is ${obj.price} rupees ${1+2+3}`
+// console.log(output)
+
+// let str = "Aftab Reshamwale"
+// let newStr = str.toUpperCase();
+// console.log(str)
+// console.log(newStr)
+
+
+// let str = "0123456";
+// console.log(str.slice(1,3));
+
+// let str1 = "Aftab"
+// let str2 = "Reshamwale"
+
+// let res = str1.concat(str2);
+// console.log(res)
+
+// let userName = prompt("Enter Your Name : ")
+// console.log("@" + userName + userName.length);
